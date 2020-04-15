6a2bc86f4bb2fefc844887657bf1cc36d436043d
klrc <1440698245@qq.com>
2020-04-15 23:06:58 +0800

    .

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages  # 这个包没有的可以pip一下
 
 setup(
     name="labvision",  # 这里是pip项目发布的名称
-    version="0.0.1",  # 版本号，数值大的会优先被pip
+    version="0.0.4",  # 版本号，数值大的会优先被pip
     keywords=("pip", "labvision", "ecust"),
     description="ecust vision lab platform",
     long_description="experimental platform for CV research.",
@@ -24,10 +24,12 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms=["all"],
-    install_requires=["torch","torchvision"]  # 这个项目需要的第三方库
+    install_requires=["torch", "torchvision", "opencv-python", "numpy"]  # 这个项目需要的第三方库
 )
 
 
 '''
     >>> python setup.py sdist 
+    >>> twine upload dist/labvision-0.0.x.tar.gz
+    >>> pip install labvision -i https://pypi.org/simple
 '''
