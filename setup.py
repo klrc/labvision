1abb7ac378c3e16d3f3dd3e8dc161a27dc707414
klrc <1440698245@qq.com>
2020-04-15 20:53:00 +0800

    .

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: klrc
+# Mail: sh@mail.ecust.edu.cn
+#############################################
+
+from setuptools import setup, find_packages  # 这个包没有的可以pip一下
+
+setup(
+    name="labvision",  # 这里是pip项目发布的名称
+    version="0.0.1",  # 版本号，数值大的会优先被pip
+    keywords=("pip", "labvision", "ecust"),
+    description="ecust vision lab platform",
+    long_description="just for fun",
+    license="MIT Licence",
+
+    url="https://github.com/LiangjunFeng/SICA",  # 项目相关文件地址，一般是github
+    author="klrc",
+    author_email="sh@mail.ecust.edu.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["numpy"]  # 这个项目需要的第三方库
+)
