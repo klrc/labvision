637811ddd1f6122d06330e89505474d46d5c6a7c
klrc <1440698245@qq.com>
2020-05-27 23:06:29 +0800

    .

--- labvision/mods/__init__.py
+++ labvision/mods/__init__.py
@@ -2,6 +2,6 @@ from .accuracy import Accuracy
 from .looper import TrainLooper, ValLooper
 from .step import Train, Validation
 from .smooth import Smooth
+from .dry import Dry
 
-
-__all__ = ['Accuracy', 'TrainLooper', 'ValLooper', 'Train', 'Validation', 'Smooth']
+__all__ = ['Accuracy', 'TrainLooper', 'ValLooper', 'Train', 'Validation', 'Smooth', 'Dry']
